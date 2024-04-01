868d83a4c00d29a09537e57387967bd0f4c93c88
Smailey Alexandre Behm <smailey.behm@gmail.com>
2024-04-01 09:12:22 -0300

    Componentes do Header, Footer, estilização e inclusão no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { secondaryFont } from '@/functions/fonts'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
@@ -14,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={secondaryFont.variable}>{children}</body>
+      <body className={secondaryFont.variable}>
+        <div className="App">
+          <Header />
+          <main className="AppBody">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   )
 }
