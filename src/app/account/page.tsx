76d138aa4a000664f763273f8353878917b28867
Smailey Alexandre Behm <smailey.behm@gmail.com>
2024-04-04 12:57:47 -0300

    Refatoração da server action que puxa as fotos, componente que mostra fotos do usuário específico

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,13 +1,36 @@
+import photosGet from '@/actions/photos-get'
+import userGet from '@/actions/user-get'
+import Feed from '@/components/feed/feed'
 import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Minha Conta'
 }
 
 export default async function AccountPage() {
+  const { data: user } = await userGet()
+  const { data } = await photosGet({ user: user?.username })
   return (
-    <main>
-      <h1>Account</h1>
-    </main>
+    <section>
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: '#444', fontSize: '1.25rem', marginBottom: '1rem' }}
+          >
+            Você ainda não postou nenhuma foto.
+          </p>
+          <Link
+            href={`/conta/postar`}
+            className="button"
+            style={{ display: 'inline-block' }}
+          >
+            Postar Foto
+          </Link>
+        </div>
+      )}
+    </section>
   )
 }
