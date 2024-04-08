0ca42d9da2167460c663ccd29325975d1f5a4e43
Smailey Alexandre Behm <smailey.behm@gmail.com>
2024-04-08 13:18:37 -0300

    server action de validação do token de login e inserção dela com efeito no provider

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,11 +1,14 @@
 'use client'
 
+import logout from '@/actions/logout'
+import validateToken from '@/actions/validate-token'
 import {
   Dispatch,
   ReactNode,
   SetStateAction,
   createContext,
   useContext,
+  useEffect,
   useState
 } from 'react'
 
@@ -38,6 +41,16 @@ export function UserContextProvider({
   user: User | null
 }) {
   const [userState, setUserState] = useState<User | null>(user)
+
+  //validação do token
+  useEffect(() => {
+    async function validate() {
+      const { ok } = await validateToken()
+      if (!ok) await logout()
+    }
+    if (userState) validate()
+  }, [userState])
+
   return (
     <UserContext.Provider value={{ user: userState, setUser: setUserState }}>
       {children}
